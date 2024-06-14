c13000c3c3ea391fa60084fba947486d90164b43
karolWisniewskiHouseBook <karol.wisniewski@housebook.solutions>
2024-06-14 21:48:53 +0200

    feat: add Hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import { Chip } from "@nextui-org/react";
+
+export default function Hero() {
+  return (
+    <>
+      <h1 className="text-white">Hey, I'm Karol Wisniewski</h1>
+      <p className="text-gray-300">
+        I am a software engineer who builds for the web with a design-oriented
+        approach. In addition to coding, I make YouTube videos, where I focus on
+        tech, and productivity.
+      </p>
+      <Chip>Chip</Chip>
+    </>
+  );
+}
